4e70f860a6ae14fe829a26fee89146686fc84538
Mark Lauter <mark.lauter@sumosoftware.com>
2019-01-04 22:55:33 -0500

    renamed VideoSample to VideoFrameCollection

---
+++ src/NetStandard/Tello.Video/VideoFrameCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tello.Video
+{
+    public sealed class VideoFrameCollection
+    {
+        public VideoFrameCollection(VideoFrame frame)
+        {
+            TimeIndex = frame.TimeIndex;
+            Add(frame);
+        }
+
+        public void Add(VideoFrame frame)
+        {
+            _frames.Add(frame);
+            _sample.Write(frame.Content, 0, frame.Size);
+            Duration += frame.Duration;
+            Size += frame.Size;
+        }
+
+
+        private readonly List<VideoFrame> _frames = new List<VideoFrame>();
+        private readonly MemoryStream _sample = new MemoryStream();
+
+        public byte[] Content => _sample.ToArray();
+        public TimeSpan Duration { get; private set; } = TimeSpan.FromSeconds(0.0);
+        public long Size { get; private set; } = 0;
+        public TimeSpan TimeIndex { get; }
+
+        public VideoFrame this[int index] => _frames[index];
+        public int Count => _frames.Count;
+    }
+}

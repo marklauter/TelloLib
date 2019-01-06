ced7923ae53e4a36716f67468b52f8e154ba7756
Mark Lauter <mark.lauter@sumosoftware.com>
2019-01-05 19:36:34 -0500

    added tello emulator console app

---
+++ src/NetStandard/Tello.Emulator.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Tello.Emulator;
+
+namespace Tello.EmulatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drone = new Drone();
+            drone.PowerOn();
+
+            Console.WriteLine("press any key to quit");
+            var key = Console.ReadKey();
+
+            drone.PowerOff();
+        }
+    }
+}

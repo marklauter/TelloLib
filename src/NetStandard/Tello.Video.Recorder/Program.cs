f833b925e5a15913d4e4eb06842582ce1bf64b61
Mark Lauter <mark.lauter@sumosoftware.com>
2019-01-07 21:02:37 -0500

    BeginReceive doesn't work

--- src/NetStandard/Tello.Video.Recorder/Program.cs
+++ src/NetStandard/Tello.Video.Recorder/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Tello.Video.Recorder
 {
@@ -18,49 +19,57 @@ namespace Tello.Video.Recorder
 
     internal class Program
     {
+        static FileStream _videoFile = new FileStream("tello.video", FileMode.OpenOrCreate);
+        static FileStream _sampleFile = new FileStream("tello.samples.json", FileMode.OpenOrCreate);
+        static long _sampleOffset = 0;
+        static Stopwatch _stopwatch = new Stopwatch();
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Tello H.264 Video Recorder based on SDK 2.0");
             Console.WriteLine("Video and sample meta data is useful for the Tello Emulator found here: ");
             Console.WriteLine("https://github.com/marklauter/TelloLib");
-            Console.WriteLine("press 'q' to quit");
 
-            IPEndPoint endpoint = null;
-
-            var videoFile = new FileStream("tello.video", FileMode.OpenOrCreate);
-            var sampleFile = new FileStream("tello.samples.json", FileMode.OpenOrCreate);
-
-            var sampleOffset = 0L;
-            var stopwatch = new Stopwatch();
-
-            var cki = Console.Read();
-            sampleFile.Write(Encoding.UTF8.GetBytes("["), 0, 1);
+            _sampleFile.Write(Encoding.UTF8.GetBytes("["), 0, 1);
             using (var client = new UdpClient(11111))
             {
-                while (cki != 13)
+                client.BeginReceive(DatagramReceived, client);
+                var wait = new SpinWait();
+                while (_stopwatch.Elapsed.TotalSeconds <= 30)
                 {
-                    var datagram = client.Receive(ref endpoint);
-                    if (!stopwatch.IsRunning)
-                        stopwatch.Start();
-
-                    videoFile.Write(datagram);
-                    var sample = new Sample
-                    {
-                        IsFrameStart = datagram.Length > 4 && datagram[0] == 0x00 && datagram[1] == 0x00 && datagram[2] == 0x00 && datagram[3] == 0x01,
-                        Offset = sampleOffset,
-                        Length = datagram.Length,
-                        TimeIndex = stopwatch.Elapsed
-                    };
-                    var sampleBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(sample));
-                    sampleFile.Write(sampleBytes, 0, sampleBytes.Length);
-
-                    sampleOffset += datagram.Length;
-
-                    cki = Console.Read();
-                    Console.Write($"\r{stopwatch.Elapsed} - {cki}");
+                    Console.Write($"\r{_stopwatch.Elapsed}");
+                    wait.SpinOnce();
                 }
             }
-            sampleFile.Write(Encoding.UTF8.GetBytes("]"), 0, 1);
+            _sampleFile.Write(Encoding.UTF8.GetBytes("]"), 0, 1);
+        }
+
+        static long _sampleCount = 0;
+        private static void DatagramReceived(IAsyncResult ar)
+        {
+            Console.Write($"\r                                   {_sampleCount++}");
+
+            if (!_stopwatch.IsRunning)
+                _stopwatch.Start();
+
+            var client = (UdpClient)ar.AsyncState;
+
+            IPEndPoint endpoint = null;
+            var datagram = client.EndReceive(ar, ref endpoint);
+            client.BeginReceive(DatagramReceived, client);
+
+            _videoFile.Write(datagram);
+            var sample = new Sample
+            {
+                IsFrameStart = datagram.Length > 4 && datagram[0] == 0x00 && datagram[1] == 0x00 && datagram[2] == 0x00 && datagram[3] == 0x01,
+                Offset = _sampleOffset,
+                Length = datagram.Length,
+                TimeIndex = _stopwatch.Elapsed
+            };
+            var sampleBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(sample) + ",");
+            _sampleFile.Write(sampleBytes, 0, sampleBytes.Length);
+
+            _sampleOffset += datagram.Length;
         }
     }
 }
